387bba1113e94f565fcb9d55366cf5682fd72f28
Han Lin <hotdogee@gmail.com>
2019-01-27 23:12:42 +0800

    Added PWA

--- quasar.conf.js
+++ quasar.conf.js
@@ -90,36 +90,20 @@ module.exports = function (ctx) {
       // workboxPluginMode: 'InjectManifest',
       // workboxOptions: {},
       manifest: {
-        // name: 'Quasar App',
-        // short_name: 'Quasar-PWA',
-        // description: 'Best PWA App in town!',
+        name: 'ANNotate',
+        short_name: 'ANNotate',
+        description: 'Protein Annotation using Recurrent Neural Networks',
         display: 'standalone',
-        orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#ffffff',
         icons: [
           {
-            'src': 'statics/icons/icon-128x128.png',
-            'sizes': '128x128',
-            'type': 'image/png'
-          },
-          {
-            'src': 'statics/icons/icon-192x192.png',
+            'src': 'statics/android-chrome-192x192.png',
             'sizes': '192x192',
             'type': 'image/png'
           },
           {
-            'src': 'statics/icons/icon-256x256.png',
-            'sizes': '256x256',
-            'type': 'image/png'
-          },
-          {
-            'src': 'statics/icons/icon-384x384.png',
-            'sizes': '384x384',
-            'type': 'image/png'
-          },
-          {
-            'src': 'statics/icons/icon-512x512.png',
+            'src': 'statics/android-chrome-512x512.png',
             'sizes': '512x512',
             'type': 'image/png'
           }
