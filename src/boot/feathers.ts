de49ddff796c304eae6661fd785c98fa21f90954
Han Lin <hotdogee@gmail.com>
2025-04-06 22:51:21 +0800

    fix: increase ackTimeout to 40s for improved Cloud Run cold start handling

--- src/boot/feathers.ts
+++ src/boot/feathers.ts
@@ -8,7 +8,7 @@ const socket = io(process.env.API_URL, {
   path: process.env.API_PATH + '/socket.io/',
   // throw errors if the server doesn't respond in time
   // Cloud Run cold start can take a while, so we set this to 30s
-  ackTimeout: 30000,
+  ackTimeout: 40000,
 })
 const client = feathers()
 // Set up Socket.io client with the socket
