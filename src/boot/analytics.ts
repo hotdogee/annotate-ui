878e4279eeabbe0b501134cf4cde6e054d727ca9
Han Lin <hotdogee@gmail.com>
2025-04-09 03:04:28 +0800

    fix: update analytics boot file to properly configure page tracking

--- src/boot/analytics.ts
+++ src/boot/analytics.ts
@@ -1,9 +1,11 @@
 import { defineBoot } from '#q-app/wrappers'
 import { configure } from 'vue-gtag'
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli-vite/boot-files
-export default defineBoot((/* { app, router, ... } */) => {
+
+export default defineBoot(({ router }) => {
   configure({
     tagId: process.env.GA_MEASUREMENT_ID || '',
+    pageTracker: {
+      router,
+    },
   })
 })
